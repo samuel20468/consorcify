e1137fe43ad4d019ef7334661fadd15e8c7a925a
NachoMolina <juanignaciomolina12@gmail.com>
2024-05-27 17:37:35 -0300

    HW - trabajamos en las funciones en equipo

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 function laCajaDePandora(numero) {
-    // proximamente escribiremos codigo aqui
+    if (numero % 2 === 0) {
+        return convertirABinario(numero);
+    } else {
+        return convertirAHexadecimal(numero);
+    }
+
+    
+}
+
+function convertirABinario(numero) {
+    if (numero === 0) {
+        return "0";
+    }
+    let binario = "";
+    while (numero > 0) {
+        binario = (numero % 2) + binario;
+        numero = Math.floor(numero / 2);
+    }
+    return binario;
+}
+
+function convertirAHexadecimal(numero) {
+    return numero.toString(16);
+}
+
+function JuanMolina (){
+    return {
+        nombre:"Juan Ignacio Molina",
+        edad: 34,
+        nacionalidad:"Argentina"
+    }
 }

a14d8a912b61f24332acfee7bba1046de6fb71a4
cuellaradriana <88406984+cuellaradriana@users.noreply.github.com>
2024-05-27 17:38:35 -0300

    My function Adriana Cuellar

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function laCajaDePandora(numero) {
-    // proximamente escribiremos codigo aqui
+    if (Number.isInteger(numero)) {
+        if (numero % 2 === 0) {
+            const binario = numero.toString(2);
+            return binario;
+        } else {
+            const hexadecimal = numero.toString(16);
+            return hexadecimal;
+        }
+    }
+    return 'El número ingresado no es un número entero';
+}
+
+function adrianaCuellar() {
+    const adriana = {
+        nombre: 'Adriana Cuellar',
+        edad: '30',
+        nacionalidad: 'venezolana',
+    };
+    return adriana;
 }

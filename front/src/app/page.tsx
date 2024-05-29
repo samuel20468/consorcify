9f68243ad1619daeecb08a1d14d12f480923d0b7
NachoMolina <juanignaciomolina12@gmail.com>
2024-05-29 11:31:28 -0300

    Creacion de componente login

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Login from "@/components/Login/Login";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>limpieza</h1>
+      <Login />
     </main>
   );
 }

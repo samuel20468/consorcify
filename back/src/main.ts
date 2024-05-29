eb81e63f3beff4cbf92adf538f8ebc6002fc070b
samuel20468 <136033542+samuel20468@users.noreply.github.com>
2024-05-28 20:46:44 -0500

    general config

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();

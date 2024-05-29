969f0e04748e817bba0961d6184400f26be144a8
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-05-29 11:19:49 -0300

    Creation of the User entity

--- back/src/modules/users/entities/user.entity.ts
+++ back/src/modules/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ length: 80, nullable: false })
+  firstName: string;
+
+  @Column({ length: 80, nullable: false })
+  lastName: string;
+
+  @Column({ length: 320, unique: true, nullable: false })
+  email: string;
+
+  @Column({ length: 72, nullable: false })
+  password: string;
+  
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ default: false })
+  isSuperAdmin: boolean;
+}

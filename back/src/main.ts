306f73de75b80bb57e4fb16e90c545832e9a6da2
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-05-30 16:40:23 -0300

    Added CORS.

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,6 +6,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,

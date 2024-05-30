0786513ceebd4a4fc8e98e8f34c0b991ab013b1a
Conejosalvaje123 <angelvazquezoriginal@gmail.com>
2024-05-30 09:46:29 -0600

    Creacion del componente Navbar y Footer

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,41 @@
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [activeSection, setActiveSection] = useState("home");
+
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const questionsRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.6,
+    };
+
+    const callback = (entries: any) => {
+      entries.forEach((entry: any) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    const sections = [homeRef.current, aboutRef.current, questionsRef.current];
+
+    sections.forEach((section) => {
+      if (section) observer.observe(section);
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        if (section) observer.unobserve(section);
+      });
+    };
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>limpieza</h1>
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backdropFilter: {
+        'none': 'none',
+        'blur': 'blur(20px)',
+      },
     },
   },
   plugins: [],

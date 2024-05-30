27591c4236282c2f4b5d08f92344acb2c0f79e5f
NachoMolina <juanignaciomolina12@gmail.com>
2024-05-30 15:10:06 -0300

    Trabajo sobre formulario de registro y modificaciones en el login

--- front/src/helpers/button.helper.ts
+++ front/src/helpers/button.helper.ts
@@ -13,6 +13,25 @@ export const loginFetch = async (UserData: ILoginData) => {
         if (!response.ok) {
             throw new Error("Error al inicar Sesion");
         }
+        const data = response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const registerFetch = async (registerData: any) => {
+    try {
+        const response = await fetch("http://localhost:3001", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(registerData),
+        });
+        if (!response.ok) {
+            throw new Error("Error al Registrarse");
+        }
         return response;
     } catch (error) {
         console.error(error);

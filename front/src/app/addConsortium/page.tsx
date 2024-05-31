5b26360a96df8e07ff96bde624d4bff166e585cf
MarcosLombardo <marcoslombardo28@gmail.com>
2024-05-31 13:53:26 -0300

    ruteo

--- front/src/app/addConsortium/page.tsx
+++ front/src/app/addConsortium/page.tsx
@@ -1,15 +1,23 @@
+import Footer from "@/components/Footer/Footer";
 import FormRegisterConsortium from "@/components/FormRegisterConsortium/FormRegisterConsortium";
 import NavbarDashboard from "@/components/NavbarDashboard/NavbarDashboard";
 import SideBarAdmin from "@/components/SidebarAdmin/SideBarAdmin";
+import SideBarSuperAdmin from "@/components/SidebarSuperAdmin/SidebarSuperAdmin";
 import { ContainerDashboard } from "@/components/ui";
 
 const AddConsortium = () => {
     return (
-        <div>
+        <div className="flex flex-col h-screen">
             <SideBarAdmin />
+            {/* <SideBarSuperAdmin /> */}
             <NavbarDashboard />
             <ContainerDashboard>
-                <FormRegisterConsortium />
+                <div className="flex items-center justify-center mt-10 bg-white rounded-lg">
+                    <FormRegisterConsortium />
+                </div>
+                <div className="absolute bottom-0 w-[80%] lg:w-[90%]">
+                    <Footer />
+                </div>
             </ContainerDashboard>
         </div>
     );

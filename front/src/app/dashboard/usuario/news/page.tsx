2d95f230956332afb26e98a96317cd967eb99b3c
NachoMolina <juanignaciomolina12@gmail.com>
2024-06-07 09:38:19 -0300

    Cracion de archivo de variables de entorno - implementacion de variables

--- front/src/app/dashboard/usuario/news/page.tsx
+++ front/src/app/dashboard/usuario/news/page.tsx
@@ -3,7 +3,7 @@ import { ContainerDashboard } from "@/components/ui";
 const News = () => {
     return (
         <div>
-            <ContainerDashboard>
+            <ContainerDashboard className="w-[90%]">
                 <h1 className="flex items-center justify-center m-auto">
                     Acá van a ir todas las noticias
                 </h1>

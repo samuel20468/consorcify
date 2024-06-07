4f83010c1a9b1d3741204ba712c5d6b10eabf71a
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-06-06 22:08:34 -0300

    Functional Units Expenses

---
+++ back/src/modules/functional-units-expenses/entities/functional-units-expense.entity.ts
@@ -0,0 +1,75 @@
+import ColumnNumericTransformer from 'src/helpers/numeric-transformer.helper';
+import { Expense } from 'src/modules/expenses/entities/expense.entity';
+import { FunctionalUnit } from 'src/modules/functional-units/entities/functional-unit.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'functional_units_expenses',
+})
+export class FunctionalUnitsExpense {
+
+  /**
+   * El ID de la expensa (UUID v4)
+   * @example "75b4566f-ddc3-4b7c-93de-4f4d5840cc34"
+   * */
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  /**
+   * El monto del prorrateo de la expensa general
+   * @example 1000.50
+   */
+  @Column('numeric', {
+    precision: 10,
+    scale: 2,
+    transformer: new ColumnNumericTransformer(),
+  })
+  monthly_expenditure: number;
+
+  /**
+   * El monto del estado de cuenta (balance) de la Unidad Funcional al momento de generar la Expensa
+   * @example 1000.50
+   */
+  @Column('numeric', {
+    precision: 10,
+    scale: 2,
+    transformer: new ColumnNumericTransformer(),
+  })
+  previous_balance: number;
+
+  /**
+   * El monto de los intereses de la Unidad Funcional por mora
+   * @example 1000.50
+   */
+  @Column('numeric', {
+    precision: 10,
+    scale: 2,
+    transformer: new ColumnNumericTransformer(),
+  })
+  interests: number;
+
+  /**
+   * El monto de la expensa mensual de la Unidad Funcional
+   * @example 1000.50
+   */
+  @Column('numeric', {
+    precision: 10,
+    scale: 2,
+    transformer: new ColumnNumericTransformer(),
+  })
+  total_amount: number;
+
+  @ManyToOne(() => FunctionalUnit, (functionalUnit) => functionalUnit.expenses)
+  @JoinColumn({ name: 'functional_unit_id' })
+  functional_unit: FunctionalUnit;
+
+  @ManyToOne(() => Expense, (expense) => expense.functionalUnitsExpenses)
+  @JoinColumn({ name: 'expense_id' })
+  expense: Expense;
+}
---
+++ back/src/modules/functional-units-expenses/functional-units-expenses.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseUUIDPipe, UseGuards } from '@nestjs/common';
+import { FunctionalUnitsExpensesService } from './functional-units-expenses.service';
+import { CreateFunctionalUnitsExpenseDto } from './dto/create-functional-units-expense.dto';
+import { UpdateFunctionalUnitsExpenseDto } from './dto/update-functional-units-expense.dto';
+import { FunctionalUnitsExpense } from './entities/functional-units-expense.entity';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/guards/auth.guard';
+
+@ApiTags('Functional Units Expenses')
+@ApiBearerAuth()
+@UseGuards(AuthGuard)
+@Controller('functional-units-expenses')
+export class FunctionalUnitsExpensesController {
+  constructor(private readonly functionalUnitsExpensesService: FunctionalUnitsExpensesService) {}
+
+  @Post()
+  async create(@Body() newFunctionalUnitsExpenseDto: CreateFunctionalUnitsExpenseDto): Promise<FunctionalUnitsExpense> {
+    return await this.functionalUnitsExpensesService.create(newFunctionalUnitsExpenseDto);
+  }
+
+  @ApiQuery({ name: 'page', type: Number, required: false })
+  @ApiQuery({ name: 'limit', type: Number, required: false })
+  @Get()
+  async findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ): Promise<FunctionalUnitsExpense[]> {
+    return await this.functionalUnitsExpensesService.findAll(+page, +limit);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.functionalUnitsExpensesService.findOne(id);
+  }
+}

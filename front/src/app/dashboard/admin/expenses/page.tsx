b684a8da3b3960af6308ee638e8fa441180e6320
NachoMolina <juanignaciomolina12@gmail.com>
2024-06-09 10:32:41 -0300

    Modificacion de pagina expensas - visualuzacion de expensas totales - implementacion de pagina de creacion de expensas

--- front/src/app/dashboard/admin/expenses/page.tsx
+++ front/src/app/dashboard/admin/expenses/page.tsx
@@ -1,24 +1,103 @@
 "use client";
 
+import { IExpense } from "@/Interfaces/Interfaces";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import AddExpenses from "@/components/addExpenses/addExpenses";
 // Estilos y componentes
-import { ContainerDashboard, Title } from "@/components/ui";
+import { Button, ContainerDashboard, Title } from "@/components/ui";
+import { FaPlus } from "react-icons/fa6";
 
 // Hooks
 import useAuth from "@/helpers/useAuth";
+import useSesion from "@/helpers/useSesion";
+import Link from "next/link";
+import { useState } from "react";
 
 // --------------------
 
 const Expense = () => {
     useAuth();
+    const { token, data } = useSesion();
+    const [expensas, setExpensas] = useState<IExpense[]>([]);
+    const [result, setResult] = useState<IExpense[]>([]);
 
-    const handleSearch = () => {};
+    const handleSearch = (query: string) => {
+        const trimmedQuery = query.trim().toLocaleLowerCase();
+
+        if (!trimmedQuery) {
+            setResult(expensas || []);
+            return;
+        }
+
+        const filteredData = (expensas || []).filter((consortium: IExpense) => {
+            return Object.values(consortium).some((value) => {
+                return (
+                    typeof value === "string" &&
+                    value.toLocaleLowerCase().includes(trimmedQuery)
+                );
+            });
+        });
+
+        setResult(filteredData);
+    };
 
     return (
-        <ContainerDashboard className="h-[90vh] text-black bg-gray-100 w-[90%]">
+        <ContainerDashboard className="h-[90vh] text-black bg-gray-100 w-[90%] gap-3">
             <Title>Expensas</Title>
-            <SearchBar onSearch={handleSearch} />
+            <div className="w-[90%] flex justify-between">
+                <SearchBar onSearch={handleSearch} />
+                <Link href="/dashboard/admin/expenses/addexpense">
+                    <Button className="w-40 items-center justify-around py-2 rounded-[40px] flex">
+                        Nueva Expensa
+                        <FaPlus />
+                    </Button>
+                </Link>
+            </div>
+            <div className="w-[90%] boder rounded">
+                <div className="w-full flex justify-around">
+                    <p className="border-b border-black">Fecha de inicio</p>
+                    <p className="border-b border-black">
+                        Fecha de Vencimiento
+                    </p>
+                    <p className="border-b border-black">Total Expensas</p>
+                    <p className="border-b border-black">Estado</p>
+                    <p className="border-b border-black">Consorcio</p>
+                </div>
+                <div className="w-full h-full">
+                    {result.length !== 0 ? (
+                        result.map((expensa) => (
+                            <div
+                                key={expensa.id}
+                                className="w-full flex justify-around"
+                            >
+                                <p className="border-b border-black">
+                                    {expensa.issue_date}
+                                </p>
+                                <p className="border-b border-black">
+                                    {expensa.expiration_date}
+                                </p>
+                                <p className="border-b border-black">
+                                    {expensa.total_amount}
+                                </p>
+                                <p className="border-b border-black">
+                                    {expensa.status}
+                                </p>
+                                <p className="border-b border-black">
+                                    {expensa.consortium.name}
+                                </p>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="w-full h-[30vh] flex justify-center items-center ">
+                            <p className="flex justify-center items-center w-full h-3/4 text-2xl">
+                                {expensas.length === 0
+                                    ? "No hay expensas para visualizar"
+                                    : "No se encontraron coincidencias"}
+                            </p>
+                        </div>
+                    )}
+                </div>
+            </div>
         </ContainerDashboard>
     );
 };
--- front/src/components/addExpenses/addExpenses.tsx
+++ front/src/components/addExpenses/addExpenses.tsx
@@ -12,6 +12,7 @@ import { Span } from "next/dist/trace";
 import { error } from "console";
 import Swal from "sweetalert2";
 import exp from "constants";
+import Link from "next/link";
 
 const addExpenses = () => {
     useAuth();
@@ -94,8 +95,13 @@ const addExpenses = () => {
 
     return (
         <div className="flex flex-col w-full items-center justify-center p-8 h-1/2">
+            <div className="w-1/2 flex justify-end">
+                <Link href="/dashboard/admin/expenses/">
+                    <Button className="w-32 py-2 rounded-[40px]">Atrás</Button>
+                </Link>
+            </div>
             <div className="p-3">
-                <h3 className="text-2xl">Nueva Expensa</h3>
+                <h3 className="text-2xl text-black">Nueva Expensa</h3>
             </div>
             <form
                 className="w-1/2 flex flex-col items-center justify-between h-full  p-8 rounded-[40px] border border-black"
@@ -103,7 +109,9 @@ const addExpenses = () => {
             >
                 <div className="w-full flex flex-col">
                     <div className="w-full flex justify-between">
-                        <Label className="w-full">Fecha de Vencimiento</Label>
+                        <Label className="w-full text-black">
+                            Fecha de Vencimiento
+                        </Label>
                         {errors.expiration_date && (
                             <span className="w-full flex items-center justify-center">
                                 {errors.expiration_date}
@@ -119,7 +127,7 @@ const addExpenses = () => {
                         onChange={handleChange}
                     />
                     <div className="w-full flex flex-col">
-                        <Label className="w-full">Consorcio</Label>
+                        <Label className="w-full text-black">Consorcio</Label>
                         <select
                             value={expense.consortium_id}
                             onChange={handleChange}

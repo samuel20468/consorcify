c2ecc25fd21d3876067ff83c32d5d31f6c997169
NachoMolina <juanignaciomolina12@gmail.com>
2024-06-09 09:35:23 -0300

    modificacion de visa pagina expensas

--- front/src/app/dashboard/admin/expenses/page.tsx
+++ front/src/app/dashboard/admin/expenses/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import SearchBar from "@/components/SearchBar/SearchBar";
 import AddExpenses from "@/components/addExpenses/addExpenses";
 // Estilos y componentes
 import { ContainerDashboard, Title } from "@/components/ui";
@@ -9,14 +10,17 @@ import useAuth from "@/helpers/useAuth";
 
 // --------------------
 
-const Claim = () => {
+const Expense = () => {
     useAuth();
+
+    const handleSearch = () => {};
 
     return (
         <ContainerDashboard className="h-[90vh] text-black bg-gray-100 w-[90%]">
             <Title>Expensas</Title>
+            <SearchBar onSearch={handleSearch} />
         </ContainerDashboard>
     );
 };
 
-export default Claim;
+export default Expense;

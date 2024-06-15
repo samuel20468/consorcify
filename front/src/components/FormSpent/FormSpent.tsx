6b962f9e82ede6f3f47a218abd4feabd85ae2170
MarcosLombardo <marcoslombardo28@gmail.com>
2024-06-15 18:30:56 -0300

    Filtros combinados en la parte de gastos

--- front/src/components/FormSpent/FormSpent.tsx
+++ front/src/components/FormSpent/FormSpent.tsx
@@ -334,7 +334,7 @@ const FormSpent = () => {
                 </div>
                 <div className="mt-5 flex justify-center">
                     <Button type="submit" className="w-1/3 py-2 rounded-[40px]">
-                        Guardar Gastos
+                        Guardar Gasto
                     </Button>
                 </div>
             </form>

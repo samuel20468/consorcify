b355afde25e4ff1029d096ac6fe6e606a936c409
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-06-17 16:56:24 -0300

    Update disable cadmin

--- back/src/modules/c-admin/c-admin.module.ts
+++ back/src/modules/c-admin/c-admin.module.ts
@@ -4,10 +4,28 @@ import { CAdminsController } from './c-admin.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CAdmin } from './entities/c-admin.entity';
 import { CAdminsRepository } from './c-admin.repository';
+import { AuthService } from '../auth/auth.service';
+import { ConsortiumsRepository } from '../consortiums/consortiums.repository';
+import { User } from '../users/entities/user.entity';
+import { PassResetTokens } from '../auth/entities/reset-token.entity';
+import { JwtService } from '@nestjs/jwt';
+import { MailsService } from '../mails/mails.service';
+import { Consortium } from '../consortiums/entities/consortium.entity';
+import { GoogleMapsService } from '../google-maps/google-maps.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CAdmin])],
+  imports: [
+    TypeOrmModule.forFeature([CAdmin, User, PassResetTokens, Consortium]),
+  ],
   controllers: [CAdminsController],
-  providers: [CAdminsService, CAdminsRepository],
+  providers: [
+    CAdminsService,
+    CAdminsRepository,
+    AuthService,
+    ConsortiumsRepository,
+    JwtService,
+    MailsService,
+    GoogleMapsService,
+  ],
 })
 export class CAdminModule {}

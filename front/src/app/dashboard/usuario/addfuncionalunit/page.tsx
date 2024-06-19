ccccc4503429aa9566158b6a737c51dba4388b0f
NachoMolina <juanignaciomolina12@gmail.com>
2024-06-19 18:42:09 -0300

    correccion de detalles

--- front/src/app/dashboard/usuario/addfuncionalunit/page.tsx
+++ front/src/app/dashboard/usuario/addfuncionalunit/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import {
-  Button,
-  ContainerDashboard,
-  Input,
-  Label,
-  Title,
+    Button,
+    ContainerDashboard,
+    Input,
+    Label,
+    Title,
 } from "@/components/ui";
 import { linkFunctionalUnit } from "@/helpers/fetch.helper.uf";
 import useAuth from "@/helpers/useAuth";
@@ -14,60 +14,84 @@ import React, { useState } from "react";
 import Swal from "sweetalert2";
 
 const AddFU = () => {
-  useAuth();
-  const router = useRouter();
-  const { token, data } = useSesion();
+    useAuth();
+    const router = useRouter();
+    const { token, data } = useSesion();
 
-  const [code, setCode] = useState<string>();
+    const [code, setCode] = useState<string>();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!code) {
+            Swal.fire({
+                title: "Error",
+                text: "Debe ingresar un codigo de la unidad funcional",
+                icon: "error",
+            });
+            return;
+        }
+        if (code.length !== 8) {
+            Swal.fire({
+                title: "Error",
+                text: "El codigo debe tener 8 caracteres",
+                icon: "error",
+            });
+            setCode("");
+            return;
+        }
 
-    try {
-      const response = await linkFunctionalUnit(data.id, token, code!);
+        try {
+            const response = await linkFunctionalUnit(data.id, token, code!);
 
-      if (response) {
-        router.push("/dashboard/usuario/information/UF");
-      } else {
-        Swal.fire({
-          title: "Error",
-          text: "No se pudo vincular la unidad funcional a la cuenta",
-          icon: "error",
-        });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+            if (response) {
+                router.push("/dashboard/usuario/information/UF");
+            } else if (response.status === 409) {
+                Swal.fire({
+                    title: "Error",
+                    text: "No se pudo vincular la unidad funcional a la cuenta",
+                    icon: "error",
+                });
+            }
+        } catch (error) {
+            Swal.fire({
+                title: "Error",
+                text: (error as Error).message,
+                icon: "error",
+            });
+        }
+    };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCode(e.target.value);
-  };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCode(e.target.value);
+    };
 
-  return (
-    <ContainerDashboard className="w-[90%] h-[90vh] items-center ">
-      <Title>Agregar unidad funcional</Title>
-      <div className="flex justify-center items-center border rounded-[40px] w-1/2 h-1/2">
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col items-center justify-center gap-2"
-        >
-          <div className="text-[20px] font-bold">
-            <Label>Codigo de la unidad funcional</Label>
-            <Input
-              type="text"
-              name="functional_units"
-              placeholder="Codigo de la unidad funcional"
-              onChange={handleChange}
-            />
-          </div>
-          <Button type="submit" className="w-full py-2 rounded-[40px]">
-            Aceptar
-          </Button>
-        </form>
-      </div>
-    </ContainerDashboard>
-  );
+    return (
+        <ContainerDashboard className="w-[90%] h-[90vh] items-center ">
+            <Title>Agregar unidad funcional</Title>
+            <div className="flex justify-center items-center border rounded-[40px] w-1/2 h-1/2">
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col items-center justify-center gap-2"
+                >
+                    <div className="text-[20px] font-bold">
+                        <Label>Codigo de la unidad funcional</Label>
+                        <Input
+                            type="text"
+                            name="functional_units"
+                            placeholder="Codigo de la unidad funcional"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <Button
+                        type="submit"
+                        className="w-full py-2 rounded-[40px]"
+                    >
+                        Aceptar
+                    </Button>
+                </form>
+            </div>
+        </ContainerDashboard>
+    );
 };
 
 export default AddFU;
--- front/src/app/dashboard/usuario/information/UF/page.tsx
+++ front/src/app/dashboard/usuario/information/UF/page.tsx
@@ -6,9 +6,13 @@ import {
     ContainerDashboard,
     Input,
     Label,
+    Select,
     Title,
 } from "@/components/ui";
-import { linkFunctionalUnit } from "@/helpers/fetch.helper.uf";
+import {
+    functionalUnitExpensesId,
+    linkFunctionalUnit,
+} from "@/helpers/fetch.helper.uf";
 import { getUserById } from "@/helpers/fetch.helper.user";
 import useAuth from "@/helpers/useAuth";
 import useSesion from "@/helpers/useSesion";
@@ -23,7 +27,10 @@ const UnidadFuncional = () => {
     const { token, data } = useSesion();
     const { haveUF, isLoading, functional_unit } = useUfSesion();
     const [user, setUser] = useState<IUser>();
+    const [functional_units, setFunctionalUnits] =
+        useState<IFunctionalUnits[]>();
     const router = useRouter();
+    const [index, setIndex] = useState<number | string>(0);
 
     useEffect(() => {
         if (!isLoading && !haveUF) {
@@ -38,6 +45,7 @@ const UnidadFuncional = () => {
                 if (response?.ok) {
                     const datos = await response.json();
                     setUser(datos);
+                    setFunctionalUnits(datos?.functional_units);
                 } else {
                     Swal.fire({
                         title: "Error",
@@ -54,53 +62,103 @@ const UnidadFuncional = () => {
         }
     }, [token]);
 
+    useEffect(() => {
+        if (user) {
+            setFunctionalUnits(user?.functional_units);
+        }
+    }, [user]);
+
+    const handleSelect = (e: any) => {
+        const index = e.target.value;
+        setIndex(index);
+    };
     return (
         <div>
             <ContainerDashboard className="w-[90%] h-[90vh]">
                 <Title>Unidad Funcional</Title>
-                <div className="flex justify-end my-2 w-[90%] gap-1">
-                    <Link href="/dashboard/usuario/addfuncionalunit">
-                        <Button className="w-44 py-2 rounded-[40px]">
-                            Agregar unidad Funcional
-                        </Button>
-                    </Link>
-                    <Link href="/dashboard">
-                        <Button className="w-32 py-2 rounded-[40px]">
-                            Volver
-                        </Button>
-                    </Link>
+                <div className="flex justify-between w-[90%]">
+                    <div className="flex w-60">
+                        <Select onChange={handleSelect}>
+                            <option value="">
+                                Selecciona la unidad funcional
+                            </option>
+                            {functional_units?.map((uf, index) => (
+                                <option key={uf.id} value={index}>
+                                    {uf.location}
+                                </option>
+                            ))}
+                        </Select>
+                    </div>
+                    <div className="flex justify-end my-2 w-[90%] gap-1">
+                        <Link href="/dashboard/usuario/addfuncionalunit">
+                            <Button className="w-44 py-2 rounded-[40px]">
+                                Agregar unidad Funcional
+                            </Button>
+                        </Link>
+                        <Link href="/dashboard">
+                            <Button className="w-32 py-2 rounded-[40px]">
+                                Volver
+                            </Button>
+                        </Link>
+                    </div>
                 </div>
-                {user?.functional_units?.length! > 0 && (
-                    <div className="w-[90%] h-[90%] border rounded-[40px] flex justify-center items-center flex-col">
-                        <div className="flex flex-col">
-                            <div className="flex">
-                                <p>Tipo: </p>
-                                <p>{user?.functional_units?.[0].type}</p>
+                {user?.functional_units?.length! > 0 && index !== "" && (
+                    <div className="w-1/2 h-[60%] border rounded-[40px] flex justify-center items-center flex-col bg-neutral-100 mt-20">
+                        <div className="w-[80%] flex flex-col gap-2">
+                            <div className="flex items-center justify-center mb-8">
+                                <p className="text-2xl text-black">
+                                    Datos de la unidad funcional
+                                </p>
                             </div>
-                            <div className="flex">
-                                <p>Locacion: </p>
-                                <p>{user?.functional_units?.[0].location}</p>
+                            <div className="flex w-full gap-2 p-3 border rounded-[40px] border-black">
+                                <p className="font-bold text-black">Tipo: </p>
+                                <p className="text-black">
+                                    {user?.functional_units?.[index].type}
+                                </p>
                             </div>
-                            <div className="flex">
-                                <p>Numero: </p>
-                                <p>{user?.functional_units?.[0].number}</p>
+                            <div className="flex w-full gap-2 p-3 border rounded-[40px] border-black">
+                                <p className="font-bold text-black">
+                                    Locacion:{" "}
+                                </p>
+                                <p className="text-black">
+                                    {user?.functional_units?.[index].location}
+                                </p>
                             </div>
-                            <div className="flex">
-                                <p>Propietario: </p>
-                                <p>{user?.functional_units?.[0].owner}</p>
+                            <div className="flex w-full gap-2 p-3 border rounded-[40px] border-black">
+                                <p className="font-bold text-black">Numero: </p>
+                                <p className="text-black">
+                                    {user?.functional_units?.[index].number}
+                                </p>
                             </div>
-                            <div className="flex">
-                                <p>Telefono del Propietario: </p>
-                                <p>
+                            <div className="flex w-full gap-2 p-3 border rounded-[40px] border-black">
+                                <p className="font-bold text-black">
+                                    Propietario:{" "}
+                                </p>
+                                <p className="text-black">
+                                    {user?.functional_units?.[index].owner}
+                                </p>
+                            </div>
+                            <div className="flex w-full gap-2 p-3 border rounded-[40px] border-black">
+                                <p className="font-bold text-black">
+                                    Telefono del Propietario:{" "}
+                                </p>
+                                <p className="text-black">
                                     {
-                                        user?.functional_units?.[0]
+                                        user?.functional_units?.[index]
                                             .owner_phone_number
                                     }
                                 </p>
                             </div>
-                            <div className="flex">
-                                <p>Email del Propietario: </p>
-                                <p>{user?.functional_units?.[0].owner_email}</p>
+                            <div className="flex w-full gap-2 p-3 border rounded-[40px] border-black">
+                                <p className="font-bold text-black">
+                                    Email del Propietario:{" "}
+                                </p>
+                                <p className="text-black">
+                                    {
+                                        user?.functional_units?.[index]
+                                            .owner_email
+                                    }
+                                </p>
                             </div>
                         </div>
                     </div>

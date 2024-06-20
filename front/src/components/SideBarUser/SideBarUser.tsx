9b9499a690f480d7427b2b414ffefa496ebb2ac2
cuellaradriana <88406984+cuellaradriana@users.noreply.github.com>
2024-06-20 00:10:54 -0300

    Hover SideBar

--- front/src/components/SideBarUser/SideBarUser.tsx
+++ front/src/components/SideBarUser/SideBarUser.tsx
@@ -1,23 +1,31 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { itemsNavbarUser } from "@/utils/data";
-import { ContainerSideBar } from "../ui";
+import Link from 'next/link';
+import { itemsNavbarUser } from '@/utils/data';
+import { ContainerSideBar } from '../ui';
+import { usePathname } from 'next/navigation';
 
 const SideBarUser = () => {
+    const pathname = usePathname();
+
     return (
         <ContainerSideBar>
             <nav className="flex flex-col h-full justify-evenly">
-                {itemsNavbarUser.map((item) => (
-                    <Link
-                        href={item.link}
-                        key={item.id}
-                        className="flex flex-col items-center text-xs lg:text-base"
-                    >
-                        <div>{item.icon}</div>
-                        <h1>{item.title}</h1>
-                    </Link>
-                ))}
+                {itemsNavbarUser.map((item) => {
+                    const isActive = pathname === item.link;
+                    return (
+                        <Link
+                            href={item.link}
+                            key={item.id}
+                            className={`flex flex-col items-center text-xs lg:text-base ${
+                                isActive ? 'bg-bluee' : ''
+                            }`}
+                        >
+                            <div>{item.icon}</div>
+                            <h1>{item.title}</h1>
+                        </Link>
+                    );
+                })}
             </nav>
         </ContainerSideBar>
     );

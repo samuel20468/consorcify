be9eb58d1fd1c0d46604a44f636c3a219ab86919
NachoMolina <juanignaciomolina12@gmail.com>
2024-06-20 14:29:20 -0300

    agregado de icono y correccion de bug

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,19 +1,20 @@
 // Metadata
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 // Stilos y componentes
-import './globals.css';
-import { Oswald } from 'next/font/google';
-import Sidebars from '@/components/Sidebars/Sidebars';
-import NavbarDashboard from '@/components/NavbarDashboard/NavbarDashboard';
-import Footer from '@/components/Footer/Footer';
-import { Mostrar, Ocultar } from '@/components/ui';
+import "./globals.css";
+import { Oswald } from "next/font/google";
+import Sidebars from "@/components/Sidebars/Sidebars";
+import NavbarDashboard from "@/components/NavbarDashboard/NavbarDashboard";
+import Footer from "@/components/Footer/Footer";
+import { Mostrar, Ocultar } from "@/components/ui";
+import { Head } from "next/document";
 
-const oswald = Oswald({ subsets: ['latin'] });
+const oswald = Oswald({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: 'Consorcify',
-    description: 'Tus administraciones en un click',
+    title: "Consorcify",
+    description: "Tus administraciones en un click",
 };
 
 export default function RootLayout({
@@ -23,6 +24,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                {/* Configurar el favicon */}
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+            </head>
             <body className={`bg-fondo text-white ${oswald.className}`}>
                 <Ocultar>
                     <Sidebars />
